bd9c2f81d4bf45f435fd41f279fb4b0ff79e053d
GustavoCavalcantii <gustavocavalcanti096@gmail.com>
2025-10-11 20:11:34 -0300

    fix: Corrige erros anteriores e remove roleValidation para implementação futura.

--- codigo_fonte/backend/src/routes/PatientRoute.ts
+++ codigo_fonte/backend/src/routes/PatientRoute.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import { PatientController } from "../controllers/PatientController";
 import { AuthMiddleware } from "../middlewares/AuthMiddleware";
-import { RoleMiddleware } from "../middlewares/RoleMiddleware";
 import { ValidateRequest } from "../middlewares/ValidateRequest";
 import { PatientDto } from "../dtos/patient/PatientDto";
 import JsonRequiredMiddleware from "../middlewares/JsonRequired";
 
 const router = Router();
 
-// Rota de criação (qualquer usuário logado)
 router.post("/", AuthMiddleware, JsonRequiredMiddleware, ValidateRequest(PatientDto, ["create"]), PatientController.createPatient);
 
 // Rotas do próprio paciente
@@ -16,7 +14,7 @@ router.get("/me", AuthMiddleware, PatientController.getMyProfile);
 router.put("/me", AuthMiddleware, JsonRequiredMiddleware, ValidateRequest(PatientDto, ["update"]), PatientController.updateMyProfile);
 
 // Rotas do administrador
-router.get("/:id", AuthMiddleware, RoleMiddleware([0]), PatientController.getPatient);
-router.put("/:id", AuthMiddleware, RoleMiddleware([0]), JsonRequiredMiddleware, ValidateRequest(PatientDto, ["update"]), PatientController.updatePatient);
+router.get("/:id", AuthMiddleware, PatientController.getPatient);
+router.put("/:id", AuthMiddleware, JsonRequiredMiddleware, ValidateRequest(PatientDto, ["update"]), PatientController.updatePatient);
 
 export default router;

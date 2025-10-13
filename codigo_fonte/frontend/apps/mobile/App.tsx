8ed5addefb7c9b890d6b84588fe350b673c70272
jp lends <jpcosta45mil@gmail.com>
2025-10-13 20:06:44 -0300

    feat: adição do app.tsx e do package que eu tinha esquecido

--- codigo_fonte/frontend/apps/mobile/App.tsx
+++ codigo_fonte/frontend/apps/mobile/App.tsx
@@ -1,20 +1,43 @@
-import { StyleSheet, View } from "react-native";
-import { Button } from "@hisius/ui";
-import { color } from "@hisius/ui/theme/colors";
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AppStackParamList, AuthStackParamList } from "../../packages/@types/navigation";
+import example from "apps/mobile/src/screens/example";
+import splash from "apps/mobile/src/screens/splash";
+import AuthNavigator from "../../packages/routers/AuthNavigator";
+import AppNavigator from "../../packages/routers/AppNavigator";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+
+      setTimeout(() => {
+        setIsAuthenticated(false);
+        setIsLoading(false);
+      }, 2000);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }} id={undefined}>
+          <Stack.Screen name="Splash" component={splash} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Button title="Clique aqui (Mobile)" onPress={() => alert("Hello Mobile!")} />
-    </View>
+    <NavigationContainer>
+      {isAuthenticated ? <AppNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: color.background,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
